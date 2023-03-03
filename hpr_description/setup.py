63fea43d353411bc5b6a797dc26e90064a0e3133
Lin Zhang <linzhank@gmail.com>
2023-03-03 13:17:57 -0600

    config description package

--- hpr_description/setup.py
+++ hpr_description/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'hpr_description'
@@ -10,13 +12,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.xacro'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='pbd0',
-    maintainer_email='linzhank@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='linzhangUCA',
+    maintainer_email='lzhang@uca.edu',
+    description='HomePlateR description package',
+    license='GPLv3',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
