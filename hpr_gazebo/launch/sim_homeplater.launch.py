c7733b11614f3037d45c74546e3e43f9a9804b16
linzhanguca <lzhang12@uca.edu>
2023-03-09 06:51:45 -0600

    init branch

--- hpr_gazebo/launch/sim_homeplater.launch.py
+++ hpr_gazebo/launch/sim_homeplater.launch.py
@@ -1,17 +1,28 @@
 from xmlrpc.client import boolean
 from ament_index_python.packages import get_package_share_path
 import launch
+from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess
+# from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import Command, LaunchConfiguration
+import launch_ros
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
     urdf_package_path = get_package_share_path("hpr_description")
+    gazebo_package_path = get_package_share_path("hpr_gazebo")
     model_path = urdf_package_path / "urdf/homeplater.urdf.xacro"
     rviz_config_path = urdf_package_path / "rviz/hpr.rviz"
+    world_path = gazebo_package_path / "worlds/another_world.sdf"
 
+    # gui_arg = DeclareLaunchArgument(
+    #     name="gui",
+    #     default_value="true",
+    #     choices=["true", "false"],
+    #     description="Flag to enable joint_state_publisher_gui",
+    # )
     model_arg = DeclareLaunchArgument(
         name="model",
         default_value=str(model_path),
@@ -32,6 +43,7 @@ def generate_launch_description():
     robot_description = ParameterValue(
         Command(["xacro ", LaunchConfiguration("model")]), value_type=str
     )
+
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -42,10 +54,18 @@ def generate_launch_description():
             }
         ],
     )
+    # Depending on gui parameter, either launch joint_state_publisher or joint_state_publisher_gui
     joint_state_publisher_node = Node(
         package="joint_state_publisher",
         executable="joint_state_publisher",
+        # condition=UnlessCondition(LaunchConfiguration("gui")),
     )
+
+    # joint_state_publisher_gui_node = Node(
+    #     package="joint_state_publisher_gui",
+    #     executable="joint_state_publisher_gui",
+    #     condition=IfCondition(LaunchConfiguration("gui")),
+    # )
     gazebo_process = ExecuteProcess(
         cmd=[
             "gazebo",
@@ -54,6 +74,7 @@ def generate_launch_description():
             "libgazebo_ros_init.so",
             "-s",
             "libgazebo_ros_factory.so",
+            str(world_path),
         ],
         output="screen",
     )
@@ -84,13 +105,15 @@ def generate_launch_description():
 
     return launch.LaunchDescription(
         [
+            # gui_arg,
             sim_time_arg,
             model_arg,
             rviz_arg,
             joint_state_publisher_node,
+            # joint_state_publisher_gui_node,
             robot_state_publisher_node,
             gazebo_process,
             spawn_entity,
-            rviz_node,
+            # rviz_node,
         ]
     )
