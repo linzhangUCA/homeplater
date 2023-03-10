03238ba57c57669e162b2b49c4a6e4952691c672
linzhanguca <lzhang12@uca.edu>
2023-03-09 22:04:51 -0600

    update urdf with lidar

--- hpr_gazebo/launch/sim_homeplater.launch.py
+++ hpr_gazebo/launch/sim_homeplater.launch.py
@@ -12,7 +12,7 @@ def generate_launch_description():
     urdf_package_path = get_package_share_path("hpr_description")
     gazebo_package_path = get_package_share_path("hpr_gazebo")
     model_path = urdf_package_path / "urdf/homeplater.urdf.xacro"
-    rviz_config_path = urdf_package_path / "rviz/hpr.rviz"
+    rviz_config_path = urdf_package_path / "rviz/hpr_gazebo.rviz"
     world_path = gazebo_package_path / "worlds/demo_world.sdf"
 
     model_arg = DeclareLaunchArgument(
@@ -96,6 +96,6 @@ def generate_launch_description():
             robot_state_publisher_node,
             gazebo_process,
             spawn_entity,
-            # rviz_node,
+            rviz_node,
         ]
     )
