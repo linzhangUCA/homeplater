fed31c114c0d206dd7f73047b35d6340364c69e7
Lin Zhang <linzhank@gmail.com>
2023-03-17 12:11:07 -0500

    update slam launch file with arguments

--- hpr_sim_slam/launch/bringup_slam.launch.py
+++ hpr_sim_slam/launch/bringup_slam.launch.py
@@ -8,8 +8,9 @@ from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     gazebo_package_path = get_package_share_path("hpr_gazebo")
-    slam_package_path = get_package_share_path("hpr_sim_slam")
-    rviz_config_path = slam_package_path / "rviz/hpr_slam.rviz"
+    sim_slam_package_path = get_package_share_path("hpr_sim_slam")
+    slam_toolbox_package_path = get_package_share_path("slam_toolbox")
+    rviz_config_path = sim_slam_package_path / "rviz/hpr_slam.rviz"
 
     sim_time_arg = DeclareLaunchArgument(
         name="use_sim_time",
@@ -17,6 +18,10 @@ def generate_launch_description():
         choices=["true", "false"],
         description="Flag to enable use simulation time",
     )
+    slam_params_file_arg = DeclareLaunchArgument(
+        name='slam_params_file',
+        default_value=str(slam_toolbox_package_path / "config/mapper_params_online_async.yaml"),
+        description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
     rviz_arg = DeclareLaunchArgument(
         name="rvizconfig",
         default_value=str(rviz_config_path),
@@ -30,17 +35,21 @@ def generate_launch_description():
     )
     launch_online_async_slam = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            str(get_package_share_path("slam_toolbox") / "launch/online_async_launch.py")
-        )
+            str(slam_toolbox_package_path / "launch/online_async_launch.py")
+        ),
+        launch_arguments={
+            'use_sim_time': LaunchConfiguration('use_sim_time'),
+            'slam_params_file': LaunchConfiguration('slam_params_file'),
+        }.items()
     )
-    
+
     robot_localization_node = Node(
         package="robot_localization",
         executable="ekf_node",
         name="ekf_filter_node",
         output="screen",
         parameters=[
-            str(slam_package_path / "config/ekf.yaml"),
+            str(sim_slam_package_path / "config/ekf.yaml"),
             {"use_sim_time": LaunchConfiguration("use_sim_time")},
         ],
     )
@@ -55,6 +64,7 @@ def generate_launch_description():
     return LaunchDescription(
         [
             sim_time_arg,
+            slam_params_file_arg,
             rviz_arg,
             hpr_gazebo,
             robot_localization_node,
